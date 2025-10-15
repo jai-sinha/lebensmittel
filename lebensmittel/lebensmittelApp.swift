1125851f34d40bf47123b3eb9ebbae804e62d248
jsinha <jaisinha219@gmail.com>
2025-10-15 14:35:38 +0200

    Initial Commit

---
+++ lebensmittel/lebensmittelApp.swift
@@ -0,0 +1,17 @@
+//
+//  lebensmittelApp.swift
+//  lebensmittel
+//
+//  Created by Jai Sinha on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct lebensmittelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
